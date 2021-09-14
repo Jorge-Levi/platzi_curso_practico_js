9df1dde76a11ae77424a2ab58f0c54f3c49897a4
Jorge Levi <tapialugardo29@gmail.com>
2021-09-14 18:19:39 -0500

    Crea una página web para calcular descuentos

--- descuentos.js
+++ descuentos.js
@@ -1,1 +1,8 @@
-console.log("Hola Mundo");
+let calcularPrecio = (precioInicial,descuento) => (precioInicial * (100 - descuento)) / 100;
+
+let precioInicial = 100;
+let descuento = 50;
+
+let precioFinal = calcularPrecio(precioInicial,descuento);
+
+console.log(`${precioFinal}`);
